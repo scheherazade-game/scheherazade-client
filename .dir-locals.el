032dbb77b6d9c851e9cd850b8c4ebeec408a184b
Kat Marchán <kzm@sykosomatic.org>
2015-04-05 21:18:48 -0700

    Added .dir-locals.el

---
+++ .dir-locals.el
@@ -0,0 +1,8 @@
+((nil . ((js2-basic-offset . 2)
+         (css-indent-level . 2)
+         (js-indent-level . 2)
+         (c-basic-offset . 2)
+         (sgml-basic-offset . 2)
+         (html-basic-offset . 2)
+         (indent-tabs-mode . nil)
+         (fill-column . 80))))
