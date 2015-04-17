1c7549e8edb3bbf141688593c14014fd0b73cba2
Kat Marchán <kzm@sykosomatic.org>
2015-04-16 17:09:54 -0700

    This is not a useful place for paper

--- lib/sz-home/index.jsx
+++ lib/sz-home/index.jsx
@@ -6,14 +6,14 @@ import Paper from "lib/material/paper";
 export default class SzHome extends React.Component {
   render() {
     return (
-      <Paper zDepth={2}>
-        <h1>Tell Me A Story</h1>
-        <p>
-          Scheherazade is an online storytelling game
-          that turns roleplaying into its own MMO.
-        </p>
-        <ButtonLink bsStyle="primary" to="play">Play Now</ButtonLink>
-      </Paper>
+        <div>
+            <h1>Tell Me A Story</h1>
+            <h4>
+              Scheherazade is an online storytelling game
+              that turns roleplaying into its own MMO.
+            </h4>
+            <ButtonLink bsStyle="primary" to="play">Play Now</ButtonLink>
+        </div>
     );
   }
 };
